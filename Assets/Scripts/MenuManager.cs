f06eff9af608f56565c83a1df050bc3972fd3aa6
Xeito <74165796+Xeito21@users.noreply.github.com>
2023-05-25 13:27:10 +0700

    Animasi coin,heart,font,prefab

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject pausePanel;
+    public void PauseGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Time.timeScale = 0f;
+        pausePanel.SetActive(true);
     }
 }
