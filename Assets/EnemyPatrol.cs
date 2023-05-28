8a8b8fb97af989f4c256de54e0c0958362ea1ffb
Ryarker <aikoi5503@gmail.com>
2023-05-28 10:29:24 +0700

    edit logic
    player take damage & enemy patrol

---
+++ Assets/EnemyPatrol.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    [SerializeField] GameObject pointA;
+    [SerializeField] GameObject pointB;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Transform currentPoint;
+    [SerializeField] float speed;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        currentPoint = pointB.transform;
+        anim.SetBool("isRunning", true);
+    }
+
+    private void Update() {
+        Vector2 point = currentPoint.position - transform.position;
+        if(currentPoint == pointB.transform){
+            rb.velocity = new Vector2(speed, 0);
+        }else{
+            rb.velocity = new Vector2(-speed, 0);
+        }
+
+        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform){
+            flip();
+            currentPoint = pointA.transform;
+        }
+        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform){
+            flip();
+            currentPoint = pointB.transform;
+        }
+    }
+    
+    private void flip () {
+        Vector3 localScale = transform.localScale;
+        localScale.x *=  -1;
+        transform.localScale = localScale;
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawWireSphere(pointA.transform.position, 0.5f);
+        Gizmos.DrawWireSphere(pointB.transform.position, 0.5f);
+
+        Gizmos.DrawLine(pointA.transform.position, pointB.transform.position);
+    }
+}
