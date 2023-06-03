153dfc16621838c2089d0aa4764b2bb90d1bb817
syabatullah <ahmadjumadiaden17@gmail.com>
2023-06-03 16:56:23 +0700

    Materi

---
+++ Assets/Materi/Script/popup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class popup : MonoBehaviour
+{
+	public GameObject PopUp;
+	public bool aktif;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+	void OnMouseDown(){
+		PopUp.SetActive(aktif);
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Materi/Script/tombol.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class tombol : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+	}
+		public void scale(float scale){
+			transform.localScale = new Vector2(1/scale,1*scale);
+		}
+		public void scene(string scene){
+			SceneManager.LoadScene (scene);
+		}
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
